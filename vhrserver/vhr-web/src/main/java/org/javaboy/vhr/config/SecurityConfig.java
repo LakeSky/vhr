e2e395d265c56a26b6960025a8e424f651844754
deozhang <759300446@qq.com>
2020-04-06 15:39:28 +0800

    删除注释

--- vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/SecurityConfig.java
+++ vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/SecurityConfig.java
@@ -30,14 +30,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * @作者 江南一点雨
- * @公众号 江南一点雨
- * @微信号 a_java_boy
- * @GitHub https://github.com/lenve
- * @博客 http://wangsong.blog.csdn.net
- * @网站 http://www.javaboy.org
- */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
